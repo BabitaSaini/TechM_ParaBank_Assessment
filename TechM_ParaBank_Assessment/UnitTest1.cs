a2ef192d6ebac261ad2af36821c5ec0a3d08cf9f
krishna <sainibabita094@gmail.com>
2026-04-07 00:04:24 +0530

    Add project files.

---
+++ TechM_ParaBank_Assessment/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace TechM_ParaBank_Assessment
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
