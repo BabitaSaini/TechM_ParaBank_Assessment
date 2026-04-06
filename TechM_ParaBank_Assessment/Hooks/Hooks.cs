335ffbd69c4900ccddd22d2503565b8b4366acd7
krishna <sainibabita094@gmail.com>
2026-04-07 04:42:11 +0530

    Test automated and Allure reporting generated

--- TechM_ParaBank_Assessment/Hooks/Hooks.cs
+++ TechM_ParaBank_Assessment/Hooks/Hooks.cs
@@ -1,10 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Allure.Commons;
+using Drivers;
+using OpenQA.Selenium;
+using Reqnroll;
 
-namespace TechM_ParaBank_Assessment.Hooks
+[Binding]
+public class Hooks
 {
-    internal class Hooks
+    private readonly ScenarioContext scenarioContext;
+    private IWebDriver driver;
+    public Hooks(ScenarioContext scenarioContext)
     {
+        this.scenarioContext = scenarioContext;
+    }
+    [BeforeScenario]
+    public void Setup()
+    {
+        DriverFactory.InitDriver();
+        driver = DriverFactory.GetDriver();
+    }
+
+    [AfterScenario]
+    public void TearDown()
+    {
+
+        if (scenarioContext.TestError != null)
+        {
+            var screenshot = ((ITakesScreenshot)driver)
+                                .GetScreenshot()
+                                .AsByteArray;
+
+            AllureLifecycle.Instance.AddAttachment(
+                "Screenshot on Failure",
+                "image/png",
+                screenshot
+            );
+        }
+        DriverFactory.QuitDriver();
     }
 }
--- TechM_ParaBank_Assessment/Utilities/WaitHelper.cs
+++ TechM_ParaBank_Assessment/Utilities/WaitHelper.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 
-namespace TechM_ParaBank_Assessment.Utilities
+namespace Utilities
 {
-    internal class WaitHelper
+    public class WaitHelper
     {
+        private IWebDriver driver;
+
+        public WaitHelper(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public IWebElement WaitForElement(By locator, int timeout = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+            return wait.Until(d => d.FindElement(locator));
+        }
+
+        public void WaitForElementToBeVisible(By locator, int timeout = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+            wait.Until(d => d.FindElement(locator).Displayed);
+        }
+
+        public void WaitForClick(By locator, int timeout = 10)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(timeout));
+            wait.Until(d => d.FindElement(locator).Enabled);
+        }
     }
 }
